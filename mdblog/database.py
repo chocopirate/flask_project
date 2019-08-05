9fbe096dd49bea31999cb24270a64295a6769de1
chocopirate <vlcek.gabriel@gmai.com>
2019-08-05 22:08:26 +0000

    jinja templates

---
+++ mdblog/database.py
@@ -0,0 +1,18 @@
+articles = {
+            1 : {
+                "title" : "jeden",
+                "content" : ""
+                },
+            2 : {
+                "title" : "dva",
+                "content" : ""
+                },
+            3 : {
+                "title" : "tri",
+                "content" : "zarábam keš"
+                },
+            4 : {
+                "title" : "štyri",
+                "content" : "päť, šesť, miniem ho hneď"
+                }
+        }
