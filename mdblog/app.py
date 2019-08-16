319f31f2797df6ac8100f53ae42f2fc8d95c1029
chocopirate <vlcek.gabriel@gmai.com>
2019-08-16 21:00:44 +0000

    added jinja templates

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import url_for
 from flask import render_template
+from flask import request
+from flask import redirect
 from .database import articles
 
 flask_app = Flask(__name__)
@@ -25,5 +28,19 @@ def view_article(art_id):
 	article = articles.get(art_id)
 	if article:
 		return render_template("article.jinja", article=article)
-	#else:
-	return render_template("article_not_found.jinja", art_id=art_id)
+	else:
+		return render_template("article_not_found.jinja", art_id=art_id)
+		
+@flask_app.route("/login/", methods=['GET'])
+def view_login():
+	return render_template("login.jinja")
+		
+@flask_app.route("/login/", methods=['POST'])
+def login_user():
+	username = request.form["username"]
+	password = request.form["password"]
+	if username == "admin" and password == "admin":
+		return redirect(url_for("view_admin"))
+	else:
+		return redirect(url_for("view_login"))
+
